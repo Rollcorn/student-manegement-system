0edf35a03ac8a25b9e25b3d2dd4b6f439059316b
denis shkandyuk <lazydaimon@gmail.com>
2022-05-04 18:39:58 +0300

    Initial commit

---
+++ src/main/java/com/shkandyuk/studentmanegementsystem/StudentManegementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shkandyuk.studentmanegementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManegementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManegementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shkandyuk/studentmanegementsystem/StudentManegementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shkandyuk.studentmanegementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManegementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
